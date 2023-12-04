efc8ed193b40521e28bc1702e7d6e5c588772f35
MrRobbertKoppenol <87547437+MrRobbertKoppenol@users.noreply.github.com>
2023-12-04 19:35:35 +0100

    Add project files.

---
+++ BlazorDekstopAndWebExample.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorDekstopAndWebExample.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
